bbe3ddf544da2d4a611d9ebbe18b81ec88db3702
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-31 22:25:50 +0200

    zadacata raboti

--- src/main/java/vezbiNiziDeliviSo3/Nizidelivost.java
+++ src/main/java/vezbiNiziDeliviSo3/Nizidelivost.java
@@ -1,9 +1,27 @@
-package vezbiNiziDeliviSo3;
+/*Напиши програма со која ќе внесуваш броеви во низа и потоа ќе ги испечатиш
+броевите кои не се деливи со 3.*/
 
+package vezbiNiziDeliviSo3;
+import java.util.Scanner;
 public class Nizidelivost {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner vnes = new Scanner(System.in);
+System.out.println("Vnesi kolku elementi ima nizata");
+int n=vnes.nextInt();
+int niza[]=new int[n];
+System.out.println("Vnesi gi elementite na nizata");
+for(int i=0; i<n; i++) {
+	System.out.println(i+" --> ");
+	niza[i]=vnes.nextInt();
+	}
+
+for(int i=0; i<niza.length; i++) {
+	if(niza[i]%3!=0)
+		System.out.println("niza["+i+"]= "+niza[i]);
+}
+
 
 	}
 
